f033407c61a645d1108563ba0dba044fe1229733
Rajesh Samal <rajesh.samal111@gmail.com>
2020-08-13 13:05:36 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=41198ed35d9e945313c799e772fd5beb&query=' + latitude+','+longitude+'&units=f'
+
+    request({url, json: true}, (error, {body} = {}) => {
+        if (error) {
+            callback('unable to connect to weather service', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0]+". Currently it is "+body.current.temperature+" degrees out. It feels like "+ body.current.feelslike+ " degrees out.")
+        }
+    })
+}
+
+module.exports = forecast
