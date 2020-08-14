1214ef1c2f00b3b818ef6e067dade3b678b9a287
Rajesh Samal <rajesh.samal111@gmail.com>
2020-08-14 12:36:38 +0530

    Forecast data updated

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=41198ed35d9e945313c799e772fd5beb&query=' + latitude+','+longitude+'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=41198ed35d9e945313c799e772fd5beb&query=' + latitude+','+longitude
 
     request({url, json: true}, (error, {body} = {}) => {
         if (error) {
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0]+". Currently it is "+body.current.temperature+" degrees out. It feels like "+ body.current.feelslike+ " degrees out.")
+            callback(undefined, body.current.weather_descriptions[0]+". Currently it is "+body.current.temperature+" degrees out. It feels like "+ body.current.feelslike+ " degrees out."
+            + "The humidity is " + body.current.humidity + "%.")
         }
     })
 }
